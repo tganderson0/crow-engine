8b221c9fac2860975b4f1d081a5d22e0e21088b8
Taylor Anderson <tganderson0@gmail.com>
2023-07-01 14:46:03 -0600

    finished lighting maps

--- res/shaders/colors.fs
+++ res/shaders/colors.fs
@@ -1,10 +1,11 @@
 #version 330 core
+out vec4 FragColor;
+
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;    
     float shininess;
-};
+}; 
 
 struct Light {
     vec3 position;
@@ -14,36 +15,31 @@ struct Light {
     vec3 specular;
 };
 
-uniform Light light;  
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
   
-uniform Material material;
-
-out vec4 FragColor;
-
-in vec3 Normal;
-in vec3 FragPos;
-
 uniform vec3 viewPos;
-uniform vec3 lightPos;
+uniform Material material;
+uniform Light light;
 
 void main()
-{    
+{
     // ambient
-    vec3 ambient  = light.ambient * material.ambient;
-
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse  = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
-}
+} 
