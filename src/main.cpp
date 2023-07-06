07055e903872578b11d00464b557e4280a632dd9
Taylor Anderson <tganderson0@gmail.com>
2023-07-05 22:00:08 -0600

    created depth buffer

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,10 @@ const std::vector<const char*> validationLayers = {
 	"VK_LAYER_KHRONOS_validation"
 };
 
+const std::vector<const char*> deviceExtensions = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
 struct QueueFamilyIndices {
 	std::optional<uint32_t> graphicsFamily;
 	std::optional<uint32_t> presentFamily;
@@ -71,6 +75,8 @@ private:
 		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
+		createSwapchain();
+		createDepthBuffer();
 	}
 
 	void mainLoop() {
@@ -83,9 +89,20 @@ private:
 		if (enableValidationLayers) {
 			instance.destroyDebugUtilsMessengerEXT(debugMessenger);
 		}
+		for (auto& imageView : imageViews)
+		{
+			device.destroyImageView(imageView);
+		}
+
+		// cleanup depth view
+		device.destroyImageView(depthView);
+		device.freeMemory(depthMemory);
+		device.destroyImage(depthImage);
+
+		device.destroySwapchainKHR(swapChain);
+		instance.destroySurfaceKHR(surface);
 
 		device.destroy();
-		instance.destroySurfaceKHR(surface);
 		instance.destroy();
 
 		glfwDestroyWindow(window);
@@ -207,6 +224,7 @@ private:
 
 	bool isDeviceSuitable(vk::PhysicalDevice device) {
 		QueueFamilyIndices indices = findQueueFamilies(device);
+
 		return indices.isComplete();
 	}
 
@@ -231,7 +249,8 @@ private:
 		auto deviceFeatures = vk::PhysicalDeviceFeatures();
 		auto deviceCreateInfo = vk::DeviceCreateInfo(vk::DeviceCreateFlags(), static_cast<uint32_t>(queueCreateInfos.size()), queueCreateInfos.data());
 		deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
-		deviceCreateInfo.enabledExtensionCount = 0;
+		deviceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+		deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
 		if (enableValidationLayers) {
 			deviceCreateInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
 			deviceCreateInfo.ppEnabledLayerNames = validationLayers.data();
@@ -274,6 +293,122 @@ private:
 		return indices;
 	}
 
+	void createSwapchain(){
+		std::vector<vk::SurfaceFormatKHR> formats = physicalDevice.getSurfaceFormatsKHR(surface);
+		if (formats.empty())
+		{
+			std::cerr << "Got no formats for the physical device's surface formats" << std::endl;
+			throw std::runtime_error("Failed during swapchain creation");
+		}
+		vk::Format format = (formats[0].format == vk::Format::eUndefined) ? vk::Format::eB8G8R8A8Unorm : formats[0].format;
+		vk::SurfaceCapabilitiesKHR surfaceCapabilities = physicalDevice.getSurfaceCapabilitiesKHR(surface);
+		vk::Extent2D swapchainExtent = chooseSwapExtent(surfaceCapabilities);
+		extent = swapchainExtent;
+
+		// The FIFO present mode is guranteed by the spec to be supported
+		vk::PresentModeKHR swapchainPresentMode = vk::PresentModeKHR::eFifo;
+		vk::SurfaceTransformFlagBitsKHR preTransform = (surfaceCapabilities.supportedTransforms & vk::SurfaceTransformFlagBitsKHR::eIdentity) ? vk::SurfaceTransformFlagBitsKHR::eIdentity : surfaceCapabilities.currentTransform;
+		vk::CompositeAlphaFlagBitsKHR compositeAlpha =
+			(surfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePreMultiplied) ? vk::CompositeAlphaFlagBitsKHR::ePreMultiplied
+			: (surfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::ePostMultiplied) ? vk::CompositeAlphaFlagBitsKHR::ePostMultiplied
+			: (surfaceCapabilities.supportedCompositeAlpha & vk::CompositeAlphaFlagBitsKHR::eInherit) ? vk::CompositeAlphaFlagBitsKHR::eInherit
+			: vk::CompositeAlphaFlagBitsKHR::eOpaque;
+
+		vk::SwapchainCreateInfoKHR swapChainCreateInfo(vk::SwapchainCreateFlagsKHR(),
+			surface,
+			surfaceCapabilities.minImageCount,
+			format,
+			vk::ColorSpaceKHR::eSrgbNonlinear,
+			swapchainExtent,
+			1,
+			vk::ImageUsageFlagBits::eColorAttachment,
+			vk::SharingMode::eExclusive,
+			{},
+			preTransform,
+			compositeAlpha,
+			swapchainPresentMode,
+			true,
+			nullptr);
+		QueueFamilyIndices queueFamilyIndices = findQueueFamilies(physicalDevice);
+		if (queueFamilyIndices.graphicsFamily.value() != queueFamilyIndices.presentFamily.value())
+		{
+			swapChainCreateInfo.imageSharingMode = vk::SharingMode::eConcurrent;
+			swapChainCreateInfo.queueFamilyIndexCount = 2;
+			uint32_t queueFamilyIndice[2] = { queueFamilyIndices.graphicsFamily.value(), queueFamilyIndices.presentFamily.value() };
+			swapChainCreateInfo.pQueueFamilyIndices = queueFamilyIndice;
+		}
+
+		swapChain = device.createSwapchainKHR(swapChainCreateInfo);
+		swapchainImages = device.getSwapchainImagesKHR(swapChain);
+		imageViews.reserve(swapchainImages.size());
+		vk::ImageViewCreateInfo imageViewCreateInfo({}, {}, vk::ImageViewType::e2D, format, {}, { vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1 });
+		for (auto image : swapchainImages)
+		{
+			imageViewCreateInfo.image = image;
+			imageViews.push_back(device.createImageView(imageViewCreateInfo));
+		}
+	}
+
+	vk::Extent2D chooseSwapExtent(const vk::SurfaceCapabilitiesKHR& capabilities) {
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+			return capabilities.currentExtent;
+		}
+		else {
+			vk::Extent2D actualExtent = { SCR_WIDTH, SCR_HEIGHT };
+			actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+			actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+			return actualExtent;
+		}
+	}
+
+	void createDepthBuffer() {
+		const vk::Format depthFormat = vk::Format::eD16Unorm;
+		vk::FormatProperties formatProperties = physicalDevice.getFormatProperties(depthFormat);
+
+		vk::ImageTiling tiling;
+		if (formatProperties.linearTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) {
+			tiling = vk::ImageTiling::eLinear;
+		}
+		else if (formatProperties.optimalTilingFeatures & vk::FormatFeatureFlagBits::eDepthStencilAttachment) {
+			tiling = vk::ImageTiling::eOptimal;
+		}
+		else {
+			throw std::runtime_error("DepthStencilAttachment is not supported for D16Unorm depth format");
+		}
+		vk::ImageCreateInfo imageCreateInfo(vk::ImageCreateFlags(),
+			vk::ImageType::e2D,
+			depthFormat,
+			vk::Extent3D(extent, 1),
+			1,
+			1,
+			vk::SampleCountFlagBits::e1,
+			tiling,
+			vk::ImageUsageFlagBits::eDepthStencilAttachment
+		);
+		depthImage = device.createImage(imageCreateInfo);
+
+		vk::PhysicalDeviceMemoryProperties memoryProperties = physicalDevice.getMemoryProperties();
+		vk::MemoryRequirements memoryRequirements = device.getImageMemoryRequirements(depthImage);
+		uint32_t typeBits = memoryRequirements.memoryTypeBits;
+		uint32_t typeIndex = uint32_t(~0);
+		for (uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++)
+		{
+			if ((typeBits & 1) && ((memoryProperties.memoryTypes[i].propertyFlags & vk::MemoryPropertyFlagBits::eDeviceLocal) == vk::MemoryPropertyFlagBits::eDeviceLocal))
+			{
+				typeIndex = i;
+				break;
+			}
+			typeBits >>= 1;
+		}
+		if (typeIndex == uint32_t(~0)) {
+			throw std::runtime_error("Failed while creating the depth buffer");
+		}
+		depthMemory = device.allocateMemory(vk::MemoryAllocateInfo(memoryRequirements.size, typeIndex));
+		device.bindImageMemory(depthImage, depthMemory, 0);
+
+		depthView = device.createImageView(vk::ImageViewCreateInfo(vk::ImageViewCreateFlags(), depthImage, vk::ImageViewType::e2D, depthFormat, {}, { vk::ImageAspectFlagBits::eDepth, 0, 1, 0, 1 }));
+	}
+
 public:
 	GLFWwindow* window;
 private:
@@ -285,8 +420,15 @@ private:
 	vk::Queue graphicsQueue;
 	vk::Queue presentQueue;
 	vk::SurfaceKHR surface;
+	vk::SwapchainKHR swapChain;
+	std::vector<vk::Image> swapchainImages;
+	vk::Image depthImage;
+	vk::DeviceMemory depthMemory;
+	vk::ImageView depthView;
+	std::vector<vk::ImageView> imageViews;
 	VkSurfaceKHR _tmpsurface;
 	PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr = dl.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
+	vk::Extent2D extent;
 };
 
 int main()
